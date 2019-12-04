19fbd39c33ce1b38d2fa0452b7dc17f355ea553e
wen <wenyuan0201@gmail.com>
2019-12-03 23:00:21 -0500

    motion layout

---
+++ app/src/main/java/com/yuan/nyctransit/AnimationActivity.kt
@@ -0,0 +1,12 @@
+package com.yuan.nyctransit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AnimationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation)
+    }
+}
