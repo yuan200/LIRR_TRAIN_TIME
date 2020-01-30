a79f62c467efbc84ba9a8edab1972a3862f0e3e4
wen <wenyuan0201@gmail.com>
2020-01-30 07:54:03 -0500

    ..

--- app/src/main/java/com/yuan/nyctransit/features/lirr/search/StopAdapter.kt
+++ app/src/main/java/com/yuan/nyctransit/features/lirr/search/StopAdapter.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.databinding.DataBindingUtil
 import com.yuan.nyctransit.R
 import com.yuan.nyctransit.databinding.RowItemBinding
 
-class StopAdapter(val stopList: List<String>): BaseAdapter(){
+class StopAdapter(val stopList: List<String>): BaseAdapter(), Filterable{
+
     private var inflater: LayoutInflater? = null
+
+    private lateinit var valueFilter: ValueFi
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         if (inflater == null) {
@@ -28,6 +33,21 @@ class StopAdapter(val stopList: List<String>): BaseAdapter(){
 
     override fun getCount() = stopList.size
 
-//    override fun getFilter(): Filter {
-//    }
+    override fun getFilter(): Filter {
+    }
+
+    private class ValueFilter: Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val result =  FilterResults()
+
+            if (constraint != null && constraint.isNotEmpty()) {
+
+            }
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
 }
