842953522a7f60382ea6a12b558248b64e6edec2
wen <wenyuan0201@gmail.com>
2020-03-02 08:46:32 -0500

    todo empty adapter

--- app/src/main/java/com/yuan/nyctransit/features/lirr/search/SearchFragment.kt
+++ app/src/main/java/com/yuan/nyctransit/features/lirr/search/SearchFragment.kt
@@ -7,13 +7,11 @@ import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.yuan.nyctransit.R
 import com.yuan.nyctransit.core.database.Stop
 import com.yuan.nyctransit.databinding.SearchFragmentBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
     private lateinit var stopList: List<Stop>
@@ -30,26 +28,29 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<SearchFragmentBinding>(inflater, R.layout.search_fragment,
-            container, false)
+        val binding = DataBindingUtil.inflate<SearchFragmentBinding>(
+            inflater, R.layout.search_fragment,
+            container, false
+        )
         val root = binding.root
-        CoroutineScope(Dispatchers.IO).launch {
-            stopList = viewModel.stopList.value!!.toList()
-            val stopAdapter = StopAdapter(stopList)
-            binding.listView.adapter = stopAdapter
-            binding.searchView.setOnQueryTextListener(
-                object: SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(query: String?): Boolean {
-                        return false
-                    }
-
-                    override fun onQueryTextChange(newText: String?): Boolean {
-                        stopAdapter.filter.filter(newText)
-                        return false
-                    }
+        stopList = mutableListOf()
+        viewModel.stopList.observe(viewLifecycleOwner, Observer {
+            stopList = it
+        })
+        val stopAdapter = StopAdapter(stopList)
+        binding.listView.adapter = stopAdapter
+        binding.searchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
                 }
-            )
-        }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    stopAdapter.filter.filter(newText)
+                    return false
+                }
+            }
+        )
         return root
     }
 }
--- app/src/main/java/com/yuan/nyctransit/features/lirr/search/SearchViewModel.kt
+++ app/src/main/java/com/yuan/nyctransit/features/lirr/search/SearchViewModel.kt
@@ -8,13 +8,17 @@ import com.yuan.nyctransit.core.database.Stop
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchViewModel(application: Application) : AndroidViewModel(application) {
 
     val stopList: MutableLiveData<MutableList<Stop>> by lazy {
-        liveData@ MutableLiveData<MutableList<Stop>>().apply {
+        MutableLiveData<MutableList<Stop>>().apply {
+            if (this.value == null) this.postValue( mutableListOf())
             CoroutineScope(Dispatchers.IO).launch {
-                this@apply.value = LirrGtfsBase.getInstance(application)!!.stopDao().allStops() as MutableList<Stop>
+                withContext(Dispatchers.Main) {
+                    this@apply.value = LirrGtfsBase.getInstance(application)!!.stopDao().allStops() as MutableList<Stop>
+                }
             }
 
         }
